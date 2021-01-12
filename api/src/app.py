96661eaa6cc630c199e964eee7de40a38a35406c
Fernandez Santiago <santiago@MBPdeFernandez.fibertel.com.ar>
2021-01-12 18:30:55 -0300

    first commit

---
+++ api/src/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'secret'
+app.config['MYSQL_DATABASE_DB'] = 'maradona'
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+
+mysql.init_app(app)
+
+@app.route('/frases')
+def get_all():
+    cur = mysql.connect().cursor()
+    cur.execute('''select * from maradona.frases''')
+    r = [dict((cur.description[i][0], value)
+                for i, value in enumerate(row)) for row in cur.fetchall()]
+    cur.close()
+    return jsonify({'frases' : r})
+
+@app.route('/frase/<int:id>')
+def get_one(id):
+    cur = mysql.connect().cursor()
+    cur.execute('''select * from maradona.frases where id = %d''' % id)
+    r = [dict((cur.description[i][0], value)
+                for i, value in enumerate(row)) for row in cur.fetchall()]
+    return jsonify({'frases' : r})
+
+@app.route('/cantidad')
+def get_last():
+    cur = mysql.connect().cursor()
+    cur.execute('''SELECT COUNT(*) FROM maradona.frases;''')
+    r = [dict((cur.description[i][0], value)
+                for i, value in enumerate(row)) for row in cur.fetchall()]
+    #return jsonify({"count": len(r)})
+    return jsonify({'filas' : r})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ app/src/app.py
@@ -0,0 +1,43 @@
+from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
+import yaml, logging, os, time, requests, json, random
+
+""" Variable Semaforo Estados en la Conversacion """
+INPUT_TEXT = 0 
+
+""" Funciones """
+def start(update, context):
+    logger.info('He recibido un comando start')
+    update.message.reply_text('¡Bienvenido al Actualizador de Compromisos %s!' % update.message.from_user.name)
+def chiste(update, context):
+    logger.info('Consultando API Frases de Maradona')
+    update.message.reply_text(get_frase())
+def get_frase():
+    num = get_random()
+    print ("El random es: % d" % num)
+    joke=requests.get('http://api:5000/frase/%s' % num)
+    data=joke.json()
+    return data['frases'][0]['frase']
+def get_random():
+    cantidad=requests.get('http://api:5000/cantidad')
+    cant_json = cantidad.json()
+    num = cant_json['filas'][0]['COUNT(*)']
+    random.randint(0, num)
+    return (random.randint(1, num))
+
+""" Main del Programa """
+if __name__ == '__main__':
+
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logger = logging.getLogger('nAutomaticBot')
+
+    """ Llave API para conectarse a Telegram """
+    updater = Updater(token="1339119499:AAG7CotDaQcPYnAIS3vR7A70qIQcSa_CbCY", use_context=True)
+    dp = updater.dispatcher
+
+    """ Handler's """
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('frase', chiste))
+
+
+    updater.start_polling()
+    updater.idle()
