bd79e6cc4e8a7163db752e8b75723ae8642929e3
Fernandez Santiago <santiago@MBPdeFernandez.fibertel.com.ar>
2021-01-16 19:28:20 -0300

    k8s & db script

--- database/init.sql
+++ database/init.sql
@@ -1,26 +1,5 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Versión 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.7.23)
-# Base de datos: maradona
-# Tiempo de Generación: 2021-01-14 14:11:42 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+CREATE DATABASE IF NOT EXISTS `maradona` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci;
 USE `maradona`;
-
 
 # Volcado de tabla frases
 # -----------------------------------------------------------
@@ -34,22 +13,9 @@ CREATE TABLE `frases` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-LOCK TABLES `frases` WRITE;
-/*!40000 ALTER TABLE `frases` DISABLE KEYS */;
-
 INSERT INTO `frases` (`frase`, `detalle`)
 VALUES
 	('X','X1'),
 	('Y','Y1');
 
-/*!40000 ALTER TABLE `frases` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
