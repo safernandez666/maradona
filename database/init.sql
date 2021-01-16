2068f9d96c921e9cdc7401fd91fd24858079d25f
Santiago Fernandez <santiagoagustinfernandez@gmail.com>
2021-01-16 16:31:51 -0300

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -19,10 +19,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+USE `maradona`;
+
 
 # Volcado de tabla frases
-# ------------------------------------------------------------
-USE maradona
+# -----------------------------------------------------------
 
 DROP TABLE IF EXISTS `frases`;
 
