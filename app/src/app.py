4a1f269bc386927cdfe0cb3ec586a4fd6c0e801f
Fernandez Santiago <santiago@MBPdeFernandez.fibertel.com.ar>
2021-01-18 18:29:03 -0300

    Readme

--- app/src/app.py
+++ app/src/app.py
@@ -4,19 +4,16 @@ import yaml, logging, os, time, requests, json, random, schedule, telegram
 """ Variables """
 token = os.getenv("TOKEN_TELEGRAM")
 chat_id = os.getenv("CHAT_ID")
-
+minutes = os.getenv("MINUTES")
 
 """ Funciones """
-def get_frase():
+def send_message():
     num = get_random()
     joke=requests.get('http://api-service:5000/frase/%s' % num)
     data=joke.json()
     msg = "Frase: %s \n" % data['frases'][0]['frase']
     msg = msg + "Detalle: %s" % data['frases'][0]['detalle']
-    return msg
-
-def send_message(token, chat_id, msg):
-    logger.info('Consultando API Frases de Maradona')
+    logger.info('···· SE ENVIO EL MENSAJE A TELGRAM ····')
     bot = telegram.Bot(token=token)
     bot.send_message(chat_id=chat_id, text=msg)
             
@@ -24,8 +21,10 @@ def get_random():
     cantidad=requests.get('http://api-service:5000/cantidad')
     cant_json = cantidad.json()
     num = cant_json['filas'][0]['COUNT(*)']
-    random.randint(0, num)
-    return (random.randint(1, num))
+    logger.info('······ SE CONSULTA CANTIDAD: %d ······' % num)
+    ran = random.randint(1, num)
+    logger.info('······ INGRESO A RANDOM ES: %d ······' % ran)
+    return (ran)
 
 """ Logging """
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -34,7 +33,7 @@ logging.getLogger('schedule').setLevel(logging.CRITICAL + 10)
 logger.info('······ WELCOME TO MARADONA BOT ······')
 
 """ Setting Time """
-schedule.every(30).minutes.do(send_message, token, chat_id, get_frase())
+schedule.every(1).minutes.do(send_message)
 
 
 while True:
